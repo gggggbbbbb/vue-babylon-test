7678d86bed43e63709d4a243fc4afd354fbfdcc7
giuliobrugnaro@gmail.com <giuliobrugnaro@gmail.com>
2019-07-26 12:04:26 +0200

    26.07.2019

---
+++ src/components/Viewer.vue
@@ -0,0 +1,16 @@
+<template>
+<div style="height: 500px">
+    <Scene style="height: 500px">
+            <Camera type="arcRotate" :radius="200" :target="[0,0,0]" :beta="Math.PI /2"></Camera>
+            <HemisphericLight></HemisphericLight>
+            <Asset src="/static/models/test.stl"  :position="[0, 0, 0]"></Asset>
+        </Scene>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'Viewer',
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vb from 'vue-babylonjs';
+Vue.use(vb);
+
 Vue.config.productionTip = false
 
 new Vue({
